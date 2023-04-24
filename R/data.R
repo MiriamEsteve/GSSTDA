048ebf7473ab38421cbd9919c3be19481077d80b
Miriam Esteve <miriam.estevec@umh.es>
2023-04-24 12:02:06 +0200

    uploaded data

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' GEO_Eset_Norm_frma_Collapsed
+#'
+#' A matrix containing gene expression data of 104 breast cancer samples and 17 healthy breast tissues.
+#'
+#' @format Gene expression matrix.
+#' \describe{
+#'   \item{GEO_Eset_Norm_frma_Collapsed}{Gene expression of 104 breast cancer samples and 17 healthy tissue samples}
+#'   ...
+#' }
+"GEO_Eset_Norm_frma_Collapsed"
+
+#' @title pData_FRMA
+#'
+#' @description A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
+#'
+#' @format Dataframe with covariate data.
+#' \describe{
+#'   \item{pData_FRMA}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
+#'   ...
+#' }
+"pData_FRMA"
