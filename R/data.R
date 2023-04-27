bc15ba204e33f65a3587f0683eb74e303489c495
Miriam Esteve <miriam.estevec@umh.es>
2023-04-27 12:56:46 +0200

    Add data

--- R/data.R
+++ R/data.R
@@ -1,21 +1,43 @@
-#' GEO_Eset_Norm_frma_Collapsed
+#' full_data
 #'
 #' A matrix containing gene expression data of 104 breast cancer samples and 17 healthy breast tissues.
 #'
 #' @format Gene expression matrix.
 #' \describe{
-#'   \item{GEO_Eset_Norm_frma_Collapsed}{Gene expression of 104 breast cancer samples and 17 healthy tissue samples}
+#'   \item{full_data}{Gene expression of 104 breast cancer samples and 17 healthy tissue samples}
 #'   ...
 #' }
-"GEO_Eset_Norm_frma_Collapsed"
+"full_data"
 
-#' @title pData_FRMA
+#' case_tag
 #'
-#' @description A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
+#' A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
 #'
 #' @format Dataframe with covariate data.
 #' \describe{
-#'   \item{pData_FRMA}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
+#'   \item{code_tag}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
 #'   ...
 #' }
-"pData_FRMA"
+"case_tag"
+
+#' survival_time
+#'
+#' A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
+#'
+#' @format Dataframe with covariate data.
+#' \describe{
+#'   \item{survival_time}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
+#'   ...
+#' }
+"survival_time"
+
+#' survival_event
+#'
+#' A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
+#'
+#' @format Dataframe with covariate data.
+#' \describe{
+#'   \item{survival_event}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
+#'   ...
+#' }
+"survival_event"
