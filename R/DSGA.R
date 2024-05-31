fb3d5a8d3e4faf17531361248e30800f6e0db0b8
MiriamEsteve <miriam.estevecampello@uchceu.es>
2024-05-31 23:57:07 +0200

    dgsa --> dsga

--- R/DSGA.R
+++ R/DSGA.R
@@ -105,9 +105,9 @@ generate_disease_component <- function(full_data, normal_space){
 }
 
 
-#' @title plot dgsa
+#' @title plot dsga
 #' @description
-#' It draws the heatmap of the dgsa result by selecting the 100 genes with
+#' It draws the heatmap of the dsga result by selecting the 100 genes with
 #' the highest variability between samples.
 #' @param selected_matrix_disease_component Disease component matrix of
 #' the selected genes that contains the disease component of all patients.
@@ -122,8 +122,8 @@ generate_disease_component <- function(full_data, normal_space){
 #' @import ComplexHeatmap
 #' @import circlize
 #' @export
-#' @return The heatmap of the dgsa result.
-plot_dgsa <- function(selected_matrix_disease_component, case_tag){
+#' @return The heatmap of the dsga result.
+plot_dsga <- function(selected_matrix_disease_component, case_tag){
   col_fun = circlize::colorRamp2(c(-4, 0,4),
                                  c("red", "black", "green"))
   row_text_size = 10
@@ -143,7 +143,7 @@ plot_dgsa <- function(selected_matrix_disease_component, case_tag){
                                                top_annotation = ha))
 }
 
-#' @title results dgsa
+#' @title results dsga
 #' @description
 #' It calculates the 100 genes with the highest variability in the matrix
 #' disease component between samples and use them to draw the heat map.
@@ -160,14 +160,14 @@ plot_dgsa <- function(selected_matrix_disease_component, case_tag){
 #' @export
 #' @return A heatmap of the 100 genes with the highest variability in the matrix
 #' disease component.
-results_dgsa <- function(matrix_disease_component, case_tag){
+results_dsga <- function(matrix_disease_component, case_tag){
   genes_sd <- apply(matrix_disease_component,1,stats::sd)
   selected_genes_sd <- names(genes_sd[order(genes_sd,decreasing = T)])[1:100]
   selected_matrix_disease_component_sd <- matrix_disease_component[selected_genes_sd,]
 
   # DT::datatable(selected_matrix_disease_component_sd)
 
-  plot_dgsa(selected_matrix_disease_component_sd, case_tag)
+  plot_dsga(selected_matrix_disease_component_sd, case_tag)
 
   return(selected_genes_sd)
 }
